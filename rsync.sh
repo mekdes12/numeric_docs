3bafd0c27bbab64116c89fcca294b48edb6ef223
Phil Austin <paustin@eos.ubc.ca>
2017-09-01 08:43:03 -0700

    add numdocs python package

--- rsync.sh
+++ rsync.sh
@@ -1,15 +1,18 @@
 #!/bin/bash -v
+#export docs_repo=/Users/phil/repos/numeric_docs
+#export labs_repo=/Users/phil/repos/numeric
+
 rsync -avz _build/* piphome:/home/phil/public_html/numeric/.
 # rsync -avz html_files/* piphome:/home/phil/public_html/numeric/.
 #Rsync -avz pdf_files/* piphome:/home/phil/public_html/numeric/.
-rsync -avz /Users/phil/repos/numeric_docs/pdf_create/_build/*pdf /Users/phil/repos/numeric_docs/pdf_files/.
-rsync -avz /Users/phil/repos/numeric_docs/pdf_files/* /Users/phil/repos/numeric_docs/pdf_files/.
-rsync -avz /Users/phil/repos/numeric_docs/html_files/* /Users/phil/repos/numeric_docs/html_files/.
+rsync -avz $docs_repo/pdf_create/_build/*pdf $docs_repo/pdf_files/.
+rsync -avz $docs_repo/pdf_files/* $docs_repo/pdf_files/.
+rsync -avz $docs_repo/html_files/* $docs_repo/html_files/.
 #
 # now copy to server
 #
-rsync -avz /Users/phil/repos/numeric_docs/pdf_create/_build/*pdf piphome:/home/phil/public_html/numeric/pdf_files/.
-rsync -avz /Users/phil/repos/numeric_docs/pdf_files/* piphome:/home/phil/public_html/numeric/pdf_files/.
-rsync -avz /Users/phil/repos/numeric_docs/html_files/* piphome:/home/phil/public_html/numeric/html_files/.
+rsync -avz $docs_repo/pdf_create/_build/*pdf piphome:/home/phil/public_html/numeric/pdf_files/.
+rsync -avz $docs_repo/pdf_files/* piphome:/home/phil/public_html/numeric/pdf_files/.
+rsync -avz $docs_repo/html_files/* piphome:/home/phil/public_html/numeric/html_files/.
 # ssh piphome 'chmod a+r /home/phil/public_html/numeric/pdf_files/*'
 # ssh piphome -v 'ls -ldt /home/phil/public_html/numeric/pdf_files/* | head -30'
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name = "numdocs",
+    py_modules=['numdocs'],
+    use_scm_version=True,
+    setup_requires=['setuptools_scm']
+)
+
+
+    
